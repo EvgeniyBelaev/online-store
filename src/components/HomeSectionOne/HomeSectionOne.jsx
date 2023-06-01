ca7bf653f32863683cbc5f7a6efc707d9238ffa6
Evgeniy Belaev <ekjek@mail.ru>
2023-06-02 02:16:30 +0300

    section one done

--- src/components/HomeSectionOne/HomeSectionOne.jsx
+++ src/components/HomeSectionOne/HomeSectionOne.jsx
@@ -1,21 +1,31 @@
 import style from './HomeSectionOne.module.css';
 import mansCloth from './img/man_cloth.jpg'
 import womansCloth from './img/woman_cloth.jpg'
+import jewerly from './img/jewerly.jpg'
 
 const HomeSectionOne = () => {
     return (
-        <section className={style.wrapper}>
-            <div className={style.container}>
-                <span className={style.text}>Женщинам</span>
-                <img src={womansCloth} alt="img" className={style.img} />
-                <div className={style.background}></div>
+        <section>
+            <div className={style.wrapper__cloth}>
+                <div className={style.container}>
+                    <span className={style.text}>Женщинам</span>
+                    <img src={womansCloth} alt="img" className={style.img} />
+                    <div className={style.background}></div>
+                </div>
+                <div className={style.container}>
+                    <div className={style.background}></div>
+                    <img src={mansCloth} alt="img" className={style.img} />
+                    <span className={style.text}>Мужчинам</span>
+                </div>
             </div>
-            <div className={style.container}>
-                <div className={style.background}></div>
-                <img src={mansCloth} alt="img" className={style.img} />
-                <span className={style.text}>Мужчинам</span>
+            <div className={style.wrapper__jewerly}>
+                <div className={style.square__one}></div>
+                <div className={style.square__two}></div>
+                <img src={jewerly} alt="img" className={style.jewerly__img} />
+                <p className={style.text}>Украшения</p>
             </div>
         </section>
+
     );
 }
 
